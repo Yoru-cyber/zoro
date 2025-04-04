546f67b71972afd665ec5d1b174b0a6fa604c403
Yoru-cyber <carlosmendez170210@gmail.com>
2025-04-03 20:34:18 -0400

    feat: added support to select output format of file, added a frame for the button

--- src/zoro/main.py
+++ src/zoro/main.py
@@ -7,28 +7,42 @@ from PIL import Image
 import customtkinter
 
 
+class MenuButton(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.select_button = customtkinter.CTkButton(
+            self, text="Select your image", command=master.get_image
+        )
+        self.scale_factor = customtkinter.CTkOptionMenu(self, values=["2x", "4x"])
+        self.file_extension = customtkinter.CTkOptionMenu(
+            self, values=["png", "jpg", "webp"]
+        )
+        self.scale_button = customtkinter.CTkButton(
+            self,
+            text="Upscale",
+            command=lambda: threading.Thread(target=master.scale_image).start(),
+        )
+        self.select_button.grid(row=0, column=0, padx=2, pady=(0, 20), sticky="w")
+        self.scale_button.grid(row=1, column=0, padx=2, pady=(0, 20), sticky="w")
+        self.scale_factor.grid(row=2, column=0, padx=2, pady=(0, 20), sticky="w")
+        self.file_extension.grid(row=3, column=0, padx=2, pady=(0, 20), sticky="w")
+
+        # add widgets onto the frame, for example:
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("800x800")
+        self.geometry("800x500")
         self.selected_image_path = ""
         self.progress = customtkinter.CTkProgressBar(
             self, orientation="horizontal", mode="indeterminate"
         )
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.select_button = customtkinter.CTkButton(
-            self, text="Select your image", command=self.get_image
-        )
-        self.scale_factor = customtkinter.CTkOptionMenu(self, values=["2x", "4x"])
-        self.scale_button = customtkinter.CTkButton(
-            self,
-            text="Upscale",
-            command=lambda: threading.Thread(target=self.scale_image).start(),
-        )
-        self.select_button.grid(row=0, column=0, padx=2, pady=(0, 20), sticky="ew")
-        self.scale_button.grid(row=0, column=1, padx=2, pady=(0, 20), sticky="ew")
-        self.scale_factor.grid(row=0, column=2, padx=2, pady=(0, 20), sticky="ew")
-
+        self.menu_button_frame = MenuButton(master=self)
+        self.file_extension = self.menu_button_frame.file_extension.get()
+        self.menu_button_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nw")
 
     def get_image(self):
         self.selected_image_path = customtkinter.filedialog.askopenfilename()
@@ -36,16 +50,16 @@ class App(customtkinter.CTk):
             self.selected_image = customtkinter.CTkImage(
                 light_image=Image.open(self.selected_image_path),
                 dark_image=Image.open(self.selected_image_path),
-                size=(500, 500),
+                size=(350, 420),
             )
             self.selected_image_label = customtkinter.CTkLabel(
                 self, text="", image=self.selected_image
             )
-            self.selected_image_label.grid(row=3, column=1, pady=10, sticky="w")
+            self.selected_image_label.grid(row=0, column=1, pady=10)
             self.selected_image_label_path = customtkinter.CTkLabel(
                 self, text=self.selected_image_path
             )
-            self.selected_image_label_path.grid(row=4, column=1, pady=10)
+            self.selected_image_label_path.grid(row=1, column=1, pady=10)
         else:
             pass
 
@@ -53,9 +67,10 @@ class App(customtkinter.CTk):
         if len(self.selected_image_path) != 0:
             self.progress.grid(row=2, column=1)
             self.progress.start()
+            file_extension = self.menu_button_frame.file_extension.get()
             waifu2xbin = f"{pathlib.Path.cwd()}/waifu2x-ncnn-vulkan-20220728-ubuntu/waifu2x-ncnn-vulkan"
             filename, old_ext = os.path.splitext(self.selected_image_path)
-            filename = filename + "_upscaled.png"
+            filename = filename + "_upscaled" + f".{file_extension}"
             filename = os.path.basename(filename)
             try:
                 command = [
@@ -67,7 +82,7 @@ class App(customtkinter.CTk):
                 ]
                 result = subprocess.run(
                     command, capture_output=True, text=True, check=True
-                ) 
+                )
                 self.progress.stop()
                 self.progress.grid_forget()
             except subprocess.CalledProcessError as e:
